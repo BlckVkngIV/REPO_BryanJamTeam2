efdf82e1d3ce637c5ff7d7474de6d53181e3ebca
Jose Miguel Ruiz Mata <mikes0174@hotmail.com>
2023-11-12 16:32:37 -0600

    FixedMovement
    Fixed the Movement for the game ( Need to Add Sprint & Slide )

--- PROJ_BrayanJamTeam2/Intermediate/Build/Win64/x64/PROJ_BrayanJamTeam2Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_1.cpp
+++ PROJ_BrayanJamTeam2/Intermediate/Build/Win64/x64/PROJ_BrayanJamTeam2Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_1.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.29.30152
+// Compiler: 14.37.32825
 #include "G:/GitHub/REPO_BryanJamTeam2/PROJ_BrayanJamTeam2/Intermediate/Build/Win64/x64/PROJ_BrayanJamTeam2Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_1.h"
