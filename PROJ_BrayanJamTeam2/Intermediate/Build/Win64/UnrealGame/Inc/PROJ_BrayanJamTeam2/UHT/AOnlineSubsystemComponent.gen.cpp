88b5ab4aeb088fcf9092adef58abe2131951105e
BlckVkngIV <57366673+BlckVkngIV@users.noreply.github.com>
2023-11-16 00:54:20 -0600

    LastCommit (BuildUnable)

---
+++ PROJ_BrayanJamTeam2/Intermediate/Build/Win64/UnrealGame/Inc/PROJ_BrayanJamTeam2/UHT/AOnlineSubsystemComponent.gen.cpp
@@ -0,0 +1,173 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "PROJ_BrayanJamTeam2/AOnlineSubsystemComponent.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeAOnlineSubsystemComponent() {}
+// Cross Module References
+	ENGINE_API UClass* Z_Construct_UClass_ACharacter_NoRegister();
+	ENGINE_API UClass* Z_Construct_UClass_UActorComponent();
+	PROJ_BRAYANJAMTEAM2_API UClass* Z_Construct_UClass_UAOnlineSubsystemComponent();
+	PROJ_BRAYANJAMTEAM2_API UClass* Z_Construct_UClass_UAOnlineSubsystemComponent_NoRegister();
+	UPackage* Z_Construct_UPackage__Script_PROJ_BrayanJamTeam2();
+// End Cross Module References
+	DEFINE_FUNCTION(UAOnlineSubsystemComponent::execJoinGameSession)
+	{
+		P_FINISH;
+		P_NATIVE_BEGIN;
+		P_THIS->JoinGameSession();
+		P_NATIVE_END;
+	}
+	DEFINE_FUNCTION(UAOnlineSubsystemComponent::execCreateGameSession)
+	{
+		P_FINISH;
+		P_NATIVE_BEGIN;
+		P_THIS->CreateGameSession();
+		P_NATIVE_END;
+	}
+	void UAOnlineSubsystemComponent::StaticRegisterNativesUAOnlineSubsystemComponent()
+	{
+		UClass* Class = UAOnlineSubsystemComponent::StaticClass();
+		static const FNameNativePtrPair Funcs[] = {
+			{ "CreateGameSession", &UAOnlineSubsystemComponent::execCreateGameSession },
+			{ "JoinGameSession", &UAOnlineSubsystemComponent::execJoinGameSession },
+		};
+		FNativeFunctionRegistrar::RegisterFunctions(Class, Funcs, UE_ARRAY_COUNT(Funcs));
+	}
+	struct Z_Construct_UFunction_UAOnlineSubsystemComponent_CreateGameSession_Statics
+	{
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam Function_MetaDataParams[];
+#endif
+		static const UECodeGen_Private::FFunctionParams FuncParams;
+	};
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UFunction_UAOnlineSubsystemComponent_CreateGameSession_Statics::Function_MetaDataParams[] = {
+		{ "ModuleRelativePath", "AOnlineSubsystemComponent.h" },
+	};
+#endif
+	const UECodeGen_Private::FFunctionParams Z_Construct_UFunction_UAOnlineSubsystemComponent_CreateGameSession_Statics::FuncParams = { (UObject*(*)())Z_Construct_UClass_UAOnlineSubsystemComponent, nullptr, "CreateGameSession", nullptr, nullptr, 0, nullptr, 0, RF_Public|RF_Transient|RF_MarkAsNative, (EFunctionFlags)0x04080401, 0, 0, METADATA_PARAMS(Z_Construct_UFunction_UAOnlineSubsystemComponent_CreateGameSession_Statics::Function_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UFunction_UAOnlineSubsystemComponent_CreateGameSession_Statics::Function_MetaDataParams)) };
+	UFunction* Z_Construct_UFunction_UAOnlineSubsystemComponent_CreateGameSession()
+	{
+		static UFunction* ReturnFunction = nullptr;
+		if (!ReturnFunction)
+		{
+			UECodeGen_Private::ConstructUFunction(&ReturnFunction, Z_Construct_UFunction_UAOnlineSubsystemComponent_CreateGameSession_Statics::FuncParams);
+		}
+		return ReturnFunction;
+	}
+	struct Z_Construct_UFunction_UAOnlineSubsystemComponent_JoinGameSession_Statics
+	{
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam Function_MetaDataParams[];
+#endif
+		static const UECodeGen_Private::FFunctionParams FuncParams;
+	};
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UFunction_UAOnlineSubsystemComponent_JoinGameSession_Statics::Function_MetaDataParams[] = {
+		{ "ModuleRelativePath", "AOnlineSubsystemComponent.h" },
+	};
+#endif
+	const UECodeGen_Private::FFunctionParams Z_Construct_UFunction_UAOnlineSubsystemComponent_JoinGameSession_Statics::FuncParams = { (UObject*(*)())Z_Construct_UClass_UAOnlineSubsystemComponent, nullptr, "JoinGameSession", nullptr, nullptr, 0, nullptr, 0, RF_Public|RF_Transient|RF_MarkAsNative, (EFunctionFlags)0x04080401, 0, 0, METADATA_PARAMS(Z_Construct_UFunction_UAOnlineSubsystemComponent_JoinGameSession_Statics::Function_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UFunction_UAOnlineSubsystemComponent_JoinGameSession_Statics::Function_MetaDataParams)) };
+	UFunction* Z_Construct_UFunction_UAOnlineSubsystemComponent_JoinGameSession()
+	{
+		static UFunction* ReturnFunction = nullptr;
+		if (!ReturnFunction)
+		{
+			UECodeGen_Private::ConstructUFunction(&ReturnFunction, Z_Construct_UFunction_UAOnlineSubsystemComponent_JoinGameSession_Statics::FuncParams);
+		}
+		return ReturnFunction;
+	}
+	IMPLEMENT_CLASS_NO_AUTO_REGISTRATION(UAOnlineSubsystemComponent);
+	UClass* Z_Construct_UClass_UAOnlineSubsystemComponent_NoRegister()
+	{
+		return UAOnlineSubsystemComponent::StaticClass();
+	}
+	struct Z_Construct_UClass_UAOnlineSubsystemComponent_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+		static const FClassFunctionLinkInfo FuncInfo[];
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam NewProp_ParentRef_MetaData[];
+#endif
+		static const UECodeGen_Private::FObjectPropertyParams NewProp_ParentRef;
+		static const UECodeGen_Private::FPropertyParamsBase* const PropPointers[];
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UECodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_UAOnlineSubsystemComponent_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_UActorComponent,
+		(UObject* (*)())Z_Construct_UPackage__Script_PROJ_BrayanJamTeam2,
+	};
+	const FClassFunctionLinkInfo Z_Construct_UClass_UAOnlineSubsystemComponent_Statics::FuncInfo[] = {
+		{ &Z_Construct_UFunction_UAOnlineSubsystemComponent_CreateGameSession, "CreateGameSession" }, // 650197078
+		{ &Z_Construct_UFunction_UAOnlineSubsystemComponent_JoinGameSession, "JoinGameSession" }, // 3829332591
+	};
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UAOnlineSubsystemComponent_Statics::Class_MetaDataParams[] = {
+		{ "BlueprintSpawnableComponent", "" },
+		{ "ClassGroupNames", "Custom" },
+		{ "IncludePath", "AOnlineSubsystemComponent.h" },
+		{ "ModuleRelativePath", "AOnlineSubsystemComponent.h" },
+	};
+#endif
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UAOnlineSubsystemComponent_Statics::NewProp_ParentRef_MetaData[] = {
+		{ "ModuleRelativePath", "AOnlineSubsystemComponent.h" },
+	};
+#endif
+	const UECodeGen_Private::FObjectPropertyParams Z_Construct_UClass_UAOnlineSubsystemComponent_Statics::NewProp_ParentRef = { "ParentRef", nullptr, (EPropertyFlags)0x0010000000000000, UECodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, 1, nullptr, nullptr, STRUCT_OFFSET(UAOnlineSubsystemComponent, ParentRef), Z_Construct_UClass_ACharacter_NoRegister, METADATA_PARAMS(Z_Construct_UClass_UAOnlineSubsystemComponent_Statics::NewProp_ParentRef_MetaData, UE_ARRAY_COUNT(Z_Construct_UClass_UAOnlineSubsystemComponent_Statics::NewProp_ParentRef_MetaData)) };
+	const UECodeGen_Private::FPropertyParamsBase* const Z_Construct_UClass_UAOnlineSubsystemComponent_Statics::PropPointers[] = {
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UAOnlineSubsystemComponent_Statics::NewProp_ParentRef,
+	};
+	const FCppClassTypeInfoStatic Z_Construct_UClass_UAOnlineSubsystemComponent_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<UAOnlineSubsystemComponent>::IsAbstract,
+	};
+	const UECodeGen_Private::FClassParams Z_Construct_UClass_UAOnlineSubsystemComponent_Statics::ClassParams = {
+		&UAOnlineSubsystemComponent::StaticClass,
+		"Engine",
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		FuncInfo,
+		Z_Construct_UClass_UAOnlineSubsystemComponent_Statics::PropPointers,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		UE_ARRAY_COUNT(FuncInfo),
+		UE_ARRAY_COUNT(Z_Construct_UClass_UAOnlineSubsystemComponent_Statics::PropPointers),
+		0,
+		0x00B000A4u,
+		METADATA_PARAMS(Z_Construct_UClass_UAOnlineSubsystemComponent_Statics::Class_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UClass_UAOnlineSubsystemComponent_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_UAOnlineSubsystemComponent()
+	{
+		if (!Z_Registration_Info_UClass_UAOnlineSubsystemComponent.OuterSingleton)
+		{
+			UECodeGen_Private::ConstructUClass(Z_Registration_Info_UClass_UAOnlineSubsystemComponent.OuterSingleton, Z_Construct_UClass_UAOnlineSubsystemComponent_Statics::ClassParams);
+		}
+		return Z_Registration_Info_UClass_UAOnlineSubsystemComponent.OuterSingleton;
+	}
+	template<> PROJ_BRAYANJAMTEAM2_API UClass* StaticClass<UAOnlineSubsystemComponent>()
+	{
+		return UAOnlineSubsystemComponent::StaticClass();
+	}
+	DEFINE_VTABLE_PTR_HELPER_CTOR(UAOnlineSubsystemComponent);
+	UAOnlineSubsystemComponent::~UAOnlineSubsystemComponent() {}
+	struct Z_CompiledInDeferFile_FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_AOnlineSubsystemComponent_h_Statics
+	{
+		static const FClassRegisterCompiledInInfo ClassInfo[];
+	};
+	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_AOnlineSubsystemComponent_h_Statics::ClassInfo[] = {
+		{ Z_Construct_UClass_UAOnlineSubsystemComponent, UAOnlineSubsystemComponent::StaticClass, TEXT("UAOnlineSubsystemComponent"), &Z_Registration_Info_UClass_UAOnlineSubsystemComponent, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(UAOnlineSubsystemComponent), 3088150210U) },
+	};
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_AOnlineSubsystemComponent_h_3919252765(TEXT("/Script/PROJ_BrayanJamTeam2"),
+		Z_CompiledInDeferFile_FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_AOnlineSubsystemComponent_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_AOnlineSubsystemComponent_h_Statics::ClassInfo),
+		nullptr, 0,
+		nullptr, 0);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ PROJ_BrayanJamTeam2/Intermediate/Build/Win64/UnrealGame/Inc/PROJ_BrayanJamTeam2/UHT/PROJ_BrayanJamTeam2GameMode.generated.h
@@ -0,0 +1,96 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "PROJ_BrayanJamTeam2GameMode.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PROJ_BRAYANJAMTEAM2_PROJ_BrayanJamTeam2GameMode_generated_h
+#error "PROJ_BrayanJamTeam2GameMode.generated.h already included, missing '#pragma once' in PROJ_BrayanJamTeam2GameMode.h"
+#endif
+#define PROJ_BRAYANJAMTEAM2_PROJ_BrayanJamTeam2GameMode_generated_h
+
+#define FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_SPARSE_DATA
+#define FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_RPC_WRAPPERS
+#define FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_ACCESSORS
+#define FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAPROJ_BrayanJamTeam2GameMode(); \
+	friend struct Z_Construct_UClass_APROJ_BrayanJamTeam2GameMode_Statics; \
+public: \
+	DECLARE_CLASS(APROJ_BrayanJamTeam2GameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/PROJ_BrayanJamTeam2"), PROJ_BRAYANJAMTEAM2_API) \
+	DECLARE_SERIALIZER(APROJ_BrayanJamTeam2GameMode)
+
+
+#define FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAPROJ_BrayanJamTeam2GameMode(); \
+	friend struct Z_Construct_UClass_APROJ_BrayanJamTeam2GameMode_Statics; \
+public: \
+	DECLARE_CLASS(APROJ_BrayanJamTeam2GameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/PROJ_BrayanJamTeam2"), PROJ_BRAYANJAMTEAM2_API) \
+	DECLARE_SERIALIZER(APROJ_BrayanJamTeam2GameMode)
+
+
+#define FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	PROJ_BRAYANJAMTEAM2_API APROJ_BrayanJamTeam2GameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(APROJ_BrayanJamTeam2GameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(PROJ_BRAYANJAMTEAM2_API, APROJ_BrayanJamTeam2GameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APROJ_BrayanJamTeam2GameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	PROJ_BRAYANJAMTEAM2_API APROJ_BrayanJamTeam2GameMode(APROJ_BrayanJamTeam2GameMode&&); \
+	PROJ_BRAYANJAMTEAM2_API APROJ_BrayanJamTeam2GameMode(const APROJ_BrayanJamTeam2GameMode&); \
+public: \
+	PROJ_BRAYANJAMTEAM2_API virtual ~APROJ_BrayanJamTeam2GameMode();
+
+
+#define FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	PROJ_BRAYANJAMTEAM2_API APROJ_BrayanJamTeam2GameMode(APROJ_BrayanJamTeam2GameMode&&); \
+	PROJ_BRAYANJAMTEAM2_API APROJ_BrayanJamTeam2GameMode(const APROJ_BrayanJamTeam2GameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(PROJ_BRAYANJAMTEAM2_API, APROJ_BrayanJamTeam2GameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APROJ_BrayanJamTeam2GameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(APROJ_BrayanJamTeam2GameMode) \
+	PROJ_BRAYANJAMTEAM2_API virtual ~APROJ_BrayanJamTeam2GameMode();
+
+
+#define FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_9_PROLOG
+#define FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_SPARSE_DATA \
+	FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_RPC_WRAPPERS \
+	FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_ACCESSORS \
+	FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_INCLASS \
+	FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_SPARSE_DATA \
+	FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_ACCESSORS \
+	FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_INCLASS_NO_PURE_DECLS \
+	FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> PROJ_BRAYANJAMTEAM2_API UClass* StaticClass<class APROJ_BrayanJamTeam2GameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_PROJ_BrayanJamTeam2_Source_PROJ_BrayanJamTeam2_PROJ_BrayanJamTeam2GameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
